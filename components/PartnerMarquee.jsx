75b876bffba2339be24a625e6584f2f767d34609
Ian Boyle <iboyle@hotmail.com>
2022-11-16 16:45:40 -0500

    updated partner marquee

--- components/PartnerMarquee.jsx
+++ components/PartnerMarquee.jsx
@@ -9,15 +9,13 @@ import shaggys from '../assets/shaggys.png'
 import shred from '../assets/shred.png'
 import skibro from '../assets/skibro.png'
 import snowrock from '../assets/snowrock.png'
-import line from '../assets/line.png'
 
 const styles = {
-  container: 'flex flex-col items-center w-full z-10 cursor-pointer -mt-10',
+  container: 'flex flex-col items-center w-full z-10 cursor-pointer -mt-20',
   marqueecontainer: 'text-[30px] lg:text-[60px] tracking-widest scroll-hidden text-gray-800/30 font-bold font-lond py-20',
   marquee:'overflow-hidden',
   marqueetext: 'mx-1 lg:mx-2 mt-4',
-  imageBox: 'flex items-center border-8 rounded-md border-black mr-10 cursor-pointer',
-  line: 'flex items-center mr-8'
+  imageBox: 'flex items-center border-4 border-black mr-10 cursor-pointer'
 }
 
 const PartnerMarquee = () => {
@@ -30,130 +28,66 @@ const PartnerMarquee = () => {
             <Image
               src={absolute}
               alt='Absolute-Snow'
-              height={150}
-              width={150}
+              height={200}
+              width={200}
             />
-          </div>
-          <div class={styles.line}>
-            <Image
-              src={line}
-              alt='Line'
-              height={170}
-              width={2}
-            />   
           </div>
           <div class={styles.imageBox}>
             <Image
               src={dynastar}
               alt='Dynastar'
-              height={150}
-              width={150}
+              height={200}
+              width={200}
             />
-          </div>
-          <div class={styles.line}>
-            <Image
-              src={line}
-              alt='Line'
-              height={170}
-              width={2}
-            />   
-          </div>         
+          </div>        
           <div class={styles.imageBox}>
             <Image
               src={faction}
               alt='Faction'
-              height={150}
-              width={150}
+              height={200}
+              width={200}
             />
-          </div>
-          <div class={styles.line}>
-            <Image
-              src={line}
-              alt='Line'
-              height={170}
-              width={2}
-            />   
-          </div>         
+          </div>        
           <div class={styles.imageBox}>
             <Image
               src={panda}
               alt='Panda'
-              height={150}
-              width={150}
+              height={200}
+              width={200}
             />
-          </div>
-          <div class={styles.line}>
-            <Image
-              src={line}
-              alt='Line'
-              height={170}
-              width={2}
-            />   
           </div>         
           <div class={styles.imageBox}>
             <Image
               src={shaggys}
               alt='Shaggys'
-              height={150}
-              width={150}
+              height={200}
+              width={200}
             />
-          </div>
-          <div class={styles.line}>
-            <Image
-              src={line}
-              alt='Line'
-              height={170}
-              width={2}
-            />   
-          </div>         
+          </div>       
           <div class={styles.imageBox}>
             <Image
               src={shred}
               alt='Shred'
-              height={150}
-              width={150}
+              height={200}
+              width={200}
             />
-          </div>
-          <div class={styles.line}>
-            <Image
-              src={line}
-              alt='Line'
-              height={170}
-              width={2}
-            />   
-          </div>         
+          </div>        
           <div class={styles.imageBox}>
             <Image
               src={skibro}
               alt='Ski Bro'
-              height={150}
-              width={150}
+              height={200}
+              width={200}
             />
-          </div>
-          <div class={styles.line}>
-            <Image
-              src={line}
-              alt='Line'
-              height={170}
-              width={2}
-            />   
-          </div>         
+          </div>       
           <div class={styles.imageBox}>
             <Image
               src={snowrock}
               alt='Snowrock'
-              height={150}
-              width={150}
+              height={200}
+              width={200}
             />
-          </div>
-          <div class={styles.line}>
-            <Image
-              src={line}
-              alt='Line'
-              height={170}
-              width={2}
-            />   
-          </div>         
+          </div>        
         </Marquee>
       </div>
     </div>
