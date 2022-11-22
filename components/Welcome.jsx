6f8dd39dd2340a03e2e964e9aee9d140b03798cd
Ian Boyle <iboyle@hotmail.com>
2022-11-21 22:38:13 -0500

    updated welcome

--- components/Welcome.jsx
+++ components/Welcome.jsx
@@ -13,9 +13,8 @@ const Welcome = () => {
     <div id='welcome' class='flex flex-col-reverse mb-20 lg:flex-row'>
       {/* Text */}
       <div class='flex flex-col text-pri items-center lg:items-start justify-center shrink'>
-          <h1 class='mainBodyHeaderStyle font-bold font-body lg:w-[75%] 2xl:w-[42%] xxl:pr-0 text-center lg:text-left lg:pl-24'>Welcome to</h1>
-          <h1 class='mainBodyHeaderStyle font-bold font-body lg:w-[75%] 2xl:w-[50%] xxl:pr-0 text-center lg:text-left lg:pl-24 pb-8'>Meta Mountain</h1>
-          <p class='mainBodyTextStyle font-medium text-center lg:text-start md:w-[90%] lg:w-[60%] xxl:w-[40%] items-center justify-center lg:justify-start lg:items-start lg:pl-24 px-8 lg:px-4 -mt-4'>{"Sassy's are a bunch of fun loving, adrenaline fueled Sasquatches that reside on Meta Mountain. A mysterious mountain with endless terrain and some of the gnarliest features in the world."}</p>
+          <h1 class='mainBodyHeaderStyle font-bold font-body lg:w-[80-%] 2xl:w-[42%] xxl:pr-0 text-center lg:text-left lg:pl-24 pb-8'>Welcome to Meta Mountain</h1>
+          <p class='mainBodyTextStyle font-medium text-center lg:text-start md:w-[90%] lg:w-[80%] xxl:w-[40%] items-center justify-center lg:justify-start lg:items-start lg:pl-24 px-8 lg:px-4 -mt-4'>{"Sassy's are a bunch of fun loving, adrenaline fueled Sasquatches that reside on Meta Mountain. A mysterious mountain with endless terrain and some of the gnarliest features in the world."}</p>
       </div>
       {/* Desktop Image */}
       <div class='flex flex-row items-center justify-center mx-6 md:mr-12 lg:mx-0 mt-24 mb-10 lg:mt-14'>
